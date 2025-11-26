8ceefbf869fac2ba30a8426ff038297f097ab260
Rubén Guillermo López Guzmán <ruben_guille27@hotmail.com>
2025-11-25 22:06:16 -0700

    Added R2D2.ChatBot .NET 6 Core Bot template with unit tests

---
+++ ChatBots/R2D2.ChatBot/R2D2.ChatBot.Tests/Dialogs/TestData/BookingDialogTestCase.cs
@@ -0,0 +1,20 @@
+﻿// Generated with Bot Builder V4 SDK Template for Visual Studio CoreBot v4.22.0
+
+using R2D2.ChatBot;
+
+namespace R2D2.ChatBot.Tests.Dialogs.TestData
+{
+    /// <summary>
+    /// A class to store test case data for <see cref="BookingDialogTests"/>.
+    /// </summary>
+    public class BookingDialogTestCase
+    {
+        public string Name { get; set; }
+
+        public BookingDetails InitialBookingDetails { get; set; }
+
+        public string[,] UtterancesAndReplies { get; set; }
+
+        public BookingDetails ExpectedBookingDetails { get; set; }
+    }
+}
---
+++ ChatBots/R2D2.ChatBot/R2D2.ChatBot/BookingDetails.cs
@@ -0,0 +1,13 @@
+﻿// Generated with Bot Builder V4 SDK Template for Visual Studio CoreBot v4.22.0
+
+namespace R2D2.ChatBot
+{
+    public class BookingDetails
+    {
+        public string Destination { get; set; }
+
+        public string Origin { get; set; }
+
+        public string TravelDate { get; set; }
+    }
+}
