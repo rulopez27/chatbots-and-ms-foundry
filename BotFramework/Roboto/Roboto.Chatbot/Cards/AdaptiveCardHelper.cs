c970066951363feb7c72581f4b378ea1555ea620
Rubén Guillermo López Guzmán <ruben_guille27@hotmail.com>
2025-11-29 17:34:32 -0700

    Refactor on AdaptiveCardHelper.cs to make it static

--- BotFramework/Roboto/Roboto.Chatbot/Cards/AdaptiveCardHelper.cs
+++ BotFramework/Roboto/Roboto.Chatbot/Cards/AdaptiveCardHelper.cs
@@ -1,32 +1,22 @@
 ﻿using Newtonsoft.Json;
 using System.IO;
-using System.Linq;
 using Microsoft.Bot.Schema;
 
 
-namespace Roboto.Bot.Cards
+namespace Roboto.Chatbot.Cards
 {
-    public class AdaptiveCardHelper
+    public static class AdaptiveCardHelper
     {
-        private readonly string _resourceName;
-        public AdaptiveCardHelper(string resourceName)
+        public static Attachment GetAdaptiveCard(string resourceFilePath)
         {
-            _resourceName = resourceName;
-        }
-        public Attachment GetAdaptiveCard()
-        {
-            string cardPath = GetType().Assembly.GetManifestResourceNames().First(name => name.EndsWith(_resourceName));
-            using (Stream stream = GetType().Assembly.GetManifestResourceStream(cardPath))
+            using (StreamReader reader = new StreamReader(resourceFilePath))
             {
-                using (StreamReader reader = new StreamReader(stream))
+                string adaptiveCard = reader.ReadToEnd();
+                return new Attachment()
                 {
-                    string adaptiveCard = reader.ReadToEnd();
-                    return new Attachment()
-                    {
-                        ContentType = "application/vnd.microsoft.card.adaptive",
-                        Content = JsonConvert.DeserializeObject(adaptiveCard, new JsonSerializerSettings { MaxDepth = null })
-                    };
-                }
+                    ContentType = "application/vnd.microsoft.card.adaptive",
+                    Content = JsonConvert.DeserializeObject(adaptiveCard, new JsonSerializerSettings { MaxDepth = null })
+                };
             }
         }
     }
