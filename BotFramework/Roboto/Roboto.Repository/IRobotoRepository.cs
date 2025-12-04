7bd894d0bf234613e5cf9fc8d78cac55efebc4e4
Ruben Lopez <ruben_guille27@hotmail.com>
2025-12-03 23:41:25 -0700

    Added IRobotoRepository.cs to Roboto.Repository project

---
+++ BotFramework/Roboto/Roboto.Repository/IRobotoRepository.cs
@@ -0,0 +1,12 @@
+using Roboto.Models;
+
+namespace Roboto.Repository
+{
+    public interface IRobotoRepository
+    {
+        Task<List<CalendarEvent>> GetAllEventsAsync();
+        Task<CalendarEvent> GetEventByIdAsync(Guid id);
+        Task AddEventAsync(CalendarEvent calendarEvent);
+        Task<List<CalendarEvent>> GetCalendarConflictsAsync(DateTime startDateTime, DateTime endDateTime);
+    }
+}
