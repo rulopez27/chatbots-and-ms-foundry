d3bd994a4e07b837f12a518cca438d8150e97ec8
Ruben Lopez <ruben_guille27@hotmail.com>
2025-12-03 23:37:24 -0700

    Added RobotoDbContext.cs project

--- BotFramework/Roboto/Roboto.Repository/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Roboto.Repository;
-
-public class Class1
-{
-
-}
---
+++ BotFramework/Roboto/Roboto.Repository/RobotoDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System.Data.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.Sqlite;
+using Roboto.Models;
+
+namespace Roboto.Repository;
+
+public class RobotoDbContext : DbContext
+{
+    public RobotoDbContext(DbContextOptions<RobotoDbContext> options) : base(options)
+    {
+    }
+
+    public virtual DbSet<CalendarEvent> CalendarEvents { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<CalendarEvent>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Title).IsRequired().HasMaxLength(200);
+            entity.Property(e => e.StartDateTime).IsRequired();
+            entity.Property(e => e.EndDateTime).IsRequired();
+            entity.Property(e => e.Details).HasMaxLength(1000);
+        });
+    }
+
+}
