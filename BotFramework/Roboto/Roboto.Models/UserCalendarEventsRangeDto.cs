34ff70e7a98efff9fcc2be3a42e44038c6f080eb
Ruben Lopez <ruben_guille27@hotmail.com>
2025-12-07 19:01:43 -0700

    Added UserCalendarEventsRangeDto.cs to Roboto.Models

---
+++ BotFramework/Roboto/Roboto.Models/UserCalendarEventsRangeDto.cs
@@ -0,0 +1,9 @@
+namespace Roboto.Models
+{
+    public class UserCalendarEventsRangeDto
+    {
+        public int UserId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
