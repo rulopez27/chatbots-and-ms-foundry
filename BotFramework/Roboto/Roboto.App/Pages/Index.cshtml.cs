0d71707b4301186fbcdf01abbb0dae9d761590c4
Ruben Lopez <ruben_guille27@hotmail.com>
2025-12-10 21:32:20 -0700

    Added Roboto.App empty project

---
+++ BotFramework/Roboto/Roboto.App/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Roboto.App.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
---
+++ BotFramework/Roboto/Roboto.App/Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Roboto.App.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
